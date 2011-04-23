89ab6d3be4d08128b1b1db07cde57d3518d687a4
ssommerfeldt <ssommerfeldt@gmail.com>
2011-04-22 21:42:59 -0600

    Added External Assembly C# Project and added reprocess button to EmailArchive Details

---
+++ TACEURO/TACEURO/Extentions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sage.Entity.Interfaces; 
+
+namespace TACEURO
+{
+    public class Extentions
+       
+    {
+         // Example of target method signature
+        public static void ReProcess(IEmailArchive emailarchive, out String result)
+        {
+
+            result = "Completed Message";
+
+        }
+    }
+}
