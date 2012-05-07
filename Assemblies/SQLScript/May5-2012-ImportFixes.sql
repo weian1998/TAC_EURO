72a0c53392ec2451b54608cf9b42c005c5d3c530
ssommerfeldt <ssommerfeldt@gmailcom>
2012-05-07 13:18:50 -0400

    SQL Scripts Update

--- Assemblies/SQLScript/May5-2012-ImportFixes.sql
+++ Assemblies/SQLScript/May5-2012-ImportFixes.sql
@@ -10,6 +10,26 @@ WHERE     (sysdba.OPPORTUNITY.ACCOUNTMANAGERID NOT IN
                           (SELECT     USERID
                             FROM          sysdba.USERINFO))
                             
+                            Update sysdba.Opportunity                      
+SET     CREATEUSER = sysdba.ACCOUNT.ACCOUNTMANAGERID 
+FROM         sysdba.OPPORTUNITY INNER JOIN
+                      sysdba.ACCOUNT ON sysdba.OPPORTUNITY.ACCOUNTID = sysdba.ACCOUNT.ACCOUNTID
+WHERE     (sysdba.OPPORTUNITY.CREATEUSER   NOT IN
+                          (SELECT     USERID
+                            FROM          sysdba.USERINFO))
+                            
+                            Update sysdba.Opportunity                      
+SET     MODIFYUSER  = sysdba.ACCOUNT.ACCOUNTMANAGERID 
+FROM         sysdba.OPPORTUNITY INNER JOIN
+                      sysdba.ACCOUNT ON sysdba.OPPORTUNITY.ACCOUNTID = sysdba.ACCOUNT.ACCOUNTID
+WHERE     (sysdba.OPPORTUNITY.MODIFYUSER    NOT IN
+                          (SELECT     USERID
+                            FROM          sysdba.USERINFO)) 
+                            
+sELECT * FROM sysdba.OPPORTUNITY  WHERE ESTIMATEDCLOSE   IS NULL                                                       
+                            
+                            
+                            
 Update sysdba.CONTACT 
 Set FIRSTNAME = '*'                            
 Where FIRSTNAME is null or FIRSTNAME = ''
